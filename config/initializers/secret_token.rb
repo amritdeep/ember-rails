bb18dcc6dadce53db4ab4d95dd3fc30c38d56094
amritdeepdhungana <amritdhungana@lftechnology.com>
2013-06-16 18:51:38 +0545

    initial EmberJs project in Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+App::Application.config.secret_token = '4b69a6fa545d5878cb7dde91a262ca9d64dbfb606cbecdb46b8d27e032bbb545422eff041dac54126d546e477f9ecf8069a0b9bdb1a673413d575f4c5bd22f89'
